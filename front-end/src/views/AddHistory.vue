e021dc6bbc5e28e48d42236db673e878aed18b0d
yuz413 <yujn413@naver.com>
2022-11-18 22:51:12 +0900

    Add input form

--- front-end/src/views/AddHistory.vue
+++ front-end/src/views/AddHistory.vue
@@ -10,7 +10,7 @@
   </header>
 
   <h2 class="page-title">Add History</h2>
-  <v-form class="container-box pb-1" ref="form">
+  <v-form class="container-box pb-1" ref="formRef">
     <div class="container-box-content">
       <!-- Date -->
       <h3 class="add-history-title">Date</h3>
@@ -58,7 +58,43 @@
       </div>
       <div class="mt-1 add-history-nav">
         <span class="add-history-sub-title">자산</span>
-        <div class="add-history-input-width">
+        <div class="d-flex add-history-input-width">
+          <v-dialog>
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" icon variant="text" class="btn-active-size mr-1">
+                <i class="bx bx-plus-circle"></i>
+              </v-btn>
+            </template>
+
+            <template v-slot:default="{ isActive }">
+              <v-card class="mx-auto" width="300">
+                <v-card-item>
+                  <v-card-title style="font-size: 1rem">+ 자산 추가</v-card-title>
+                </v-card-item>
+
+                <v-card-text>
+                  <v-form ref="newAssetsRef">
+                    <v-text-field
+                      placeholder="자산명을 입력하세요."
+                      v-model="newAssets"
+                      variant="outlined"
+                      density="compact"
+                      :rules="assetsRules"
+                      required
+                      clearable
+                    ></v-text-field>
+                  </v-form>
+                </v-card-text>
+
+                <div class="d-flex justify-content-end mx-5 mb-4">
+                  <v-btn class="btn-sm mr-2" @click="isActive.value = false">Close</v-btn>
+                  <v-btn class="btn-sm" color="success" @click="addAssetsVaildate(isActive)"
+                    >Save</v-btn
+                  >
+                </div>
+              </v-card>
+            </template>
+          </v-dialog>
           <v-autocomplete
             v-model="assets"
             :rules="assetsRules"
@@ -73,7 +109,43 @@
       </div>
       <div class="mt-1 add-history-nav">
         <span class="add-history-sub-title">카테고리</span>
-        <div class="add-history-input-width">
+        <div class="d-flex add-history-input-width">
+          <v-dialog>
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" icon variant="text" class="btn-active-size mr-1">
+                <i class="bx bx-plus-circle"></i>
+              </v-btn>
+            </template>
+
+            <template v-slot:default="{ isActive }">
+              <v-card class="mx-auto" width="300">
+                <v-card-item>
+                  <v-card-title style="font-size: 1rem">+ 카테고리 추가</v-card-title>
+                </v-card-item>
+
+                <v-card-text>
+                  <v-form ref="newCategoryRef">
+                    <v-text-field
+                      placeholder="카테고리명을 입력하세요."
+                      v-model="newCategory"
+                      variant="outlined"
+                      density="compact"
+                      :rules="categoryRules"
+                      required
+                      clearable
+                    ></v-text-field>
+                  </v-form>
+                </v-card-text>
+
+                <div class="d-flex justify-content-end mx-5 mb-4">
+                  <v-btn class="btn-sm mr-2" @click="isActive.value = false">Close</v-btn>
+                  <v-btn class="btn-sm" color="success" @click="addCategoryVaildate(isActive)"
+                    >Save</v-btn
+                  >
+                </div>
+              </v-card>
+            </template>
+          </v-dialog>
           <v-autocomplete
             v-model="category"
             :rules="categoryRules"
@@ -89,7 +161,7 @@
       <div>
         <p class="add-history-sub-title mb-2">메모</p>
         <v-textarea
-          :model-value="memo"
+          v-model="memo"
           :rules="memoRules"
           variant="solo"
           rows="2"
@@ -101,7 +173,7 @@
     </div>
 
     <div class="d-flex justify-content-evenly mb-3">
-      <v-btn @click="validate" color="success" rounded="lg" style="width: 35%">SAVE</v-btn>
+      <v-btn @click="formValidate" color="success" rounded="lg" style="width: 35%">SAVE</v-btn>
       <v-btn @click="reset" color="warning" rounded="lg" style="width: 35%">RESET</v-btn>
     </div>
   </v-form>
@@ -133,19 +205,21 @@ export default defineComponent({
         (v: string) => /^[0-9]*$/.test(v) || 'Only number',
       ],
       assets: '',
+      newAssets: '',
       assetsItems: ['하나신용카드', '하나체크카드', '국민카드', '신한카드'],
       assetsRules: [
         (v: string) => !!v || 'Assets are required',
         (v: string) => v.length <= 12 || 'Less than 12 characters',
       ],
       category: '',
+      newCategory: '',
       categoryItems: ['식비', '교통비', '생활비', '기타'],
       categoryRules: [
         (v: string) => !!v || 'Category is required',
         (v: string) => v.length <= 12 || 'Less than 12 characters',
       ],
       memo: '',
-      memoRules: [(v: string) => v.length <= 30 || 'Less than 30 characters'],
+      memoRules: [(v: string) => v == null || v.length <= 30 || 'Less than 30 characters'],
     };
   },
 
@@ -169,29 +243,72 @@ export default defineComponent({
       return `${year}/${newMonth}/${newDay}`;
     },
 
-    async validate() {
-      const { valid } = await (this.$refs as any).form.validate();
-
-      if (valid) alert('Form is valid');
+    async formValidate() {
+      const { valid } = await (this.$refs as any).formRef.validate();
+      if (valid)
+        alert(
+          `Form is valid
+          이전날짜: ${this.date}
+          날짜: ${this.newDate}
+          내역: ${this.title}
+          금액: ${this.amount}
+          자산: ${this.assets}
+          카테고리: ${this.category}
+          메모: ${this.memo}`,
+        );
+    },
+    async addAssetsVaildate(isActive: any) {
+      const { valid } = await (this.$refs as any).newAssetsRef.validate();
+      if (valid) {
+        alert(`New Assets are valid: ${this.newAssets}`);
+        this.assetsItems.push(this.newAssets);
+        this.newAssets = '';
+        isActive.value = false;
+      }
+    },
+    async addCategoryVaildate(isActive: any) {
+      const { valid } = await (this.$refs as any).newCategoryRef.validate();
+      if (valid) {
+        alert(`New Category are valid: ${this.newCategory}`);
+        this.categoryItems.push(this.newCategory);
+        this.newCategory = '';
+        isActive.value = false;
+      }
     },
     reset() {
-      (this.$refs as any).form.reset();
+      (this.$refs as any).formRef.reset();
+      this.date = '';
+      this.newDate = '';
+
+      alert(
+        `Form is reset
+          이전날짜: ${this.date}
+          날짜: ${this.newDate}
+          내역: ${this.title}
+          금액: ${this.amount}
+          자산: ${this.assets}
+          카테고리: ${this.category}
+          메모: ${this.memo}`,
+      );
     },
   },
 
   watch: {
-    newDate() {
-      console.log('date', this.newDate);
-    },
-    autoUpdate() {
-      console.log('autoupdate', this.autoUpdate);
-    },
-    title() {
-      console.log('title', this.title);
-    },
-    amount() {
-      console.log('amount', this.amount);
-    },
+    // newDate() {
+    //   console.log('date', this.newDate);
+    // },
+    // autoUpdate() {
+    //   console.log('autoupdate', this.autoUpdate);
+    // },
+    // title() {
+    //   console.log('title', this.title);
+    // },
+    // amount() {
+    //   console.log('amount', this.amount);
+    // },
+    // memo() {
+    //   console.log('memo', this.memo);
+    // },
   },
 });
 </script>
