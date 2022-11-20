98d1d320482dd20a02f9186d73099aea6791ff76
ryusuz <ryusuzn@gmail.com>
2022-11-20 23:55:43 +0900

    최근 내역 조회, 특정 내역 디테일 조회, 월별 내역 조회, 연/월별 수입/지출액 조회 수정 및 구현

--- server/routes/eatcoin-api/histories.js
+++ server/routes/eatcoin-api/histories.js
@@ -1,76 +1,148 @@
 const { json } = require('body-parser');
-var express = require('express');
+const express = require('express');
 const morgan = require('morgan');
-var router = express.Router();
-var db = require('../../lib/config');
+const router = express.Router();
+const db = require('../../lib/config');
+const getDate = require('../../lib/etc');
 
 router.use(morgan("combined"));
 
-var from = ' from histories';
-var where = ' where hid = ?';
+const from = ' from histories';
+const where = ' where hid = ?';
 
 // todo: 에러 핸들링!!!
 
-// 1-1. 내역 조회
+// 1-1. 최근 내역 조회
 router.get('/', function(req, res, next) {
-    console.log("find all");
+    console.log("find recent");
 
-    db.query('select h.hid, h.title, h.amount, h.payment_date, h.category, c.name, c.type, c.image from histories as h inner join categories as c on h.category = c.cid order by h.hid asc', (err, results, fds) => {
-    if (err) throw err;
+    let list = [];
+    const date = getDate().slice(2);
+    const sql = `select * from histories as h inner join categories as c on h.category = c.cid where payment_date >= "${date}" order by h.hid asc`;
 
-    var list = [];
-
-    for(var i = 0; i < results.length; i++) {
-        var obj = {
-            hid : 1,
-            title : "",
-            amount : 1,
-            payment_date : "",
-            category : {}
-        };
-
-        var cate = {
-            cid : 1,
-            name : "",
-            type : "",
-            image : ""
-        };
-
-        for(var key in results[i]) {
-            if (key == 'cid' || key == 'name' || key == 'type' || key == 'image') {
-                cate[key] = results[i][key];
-                continue;
-            }
-
-            obj[key] = results[i][key];
-        }
-        
-        obj['category'] = cate;
-        // console.log(obj);
-        list.push(obj);
-
-        console.log(list[i]);
-    }
-
-    res.json(list);
-    
-    })
-});
-
-// 1-2. 특정 내역 조회
-router.get('/:id', (req, res, next) => {
-    console.log("find one");
-
-    const { id } = req.params;
-    var query = db.format('select *' + from + where, [id]);
-
-    db.query(query, (err, results, fds) => {
+    db.query(sql, (err, result) => {
         if (err) throw err;
 
-        console.log(results);
-        res.json(results);
+        result.map((data) => {
+            let { ...history } = {
+                hid : data.hid,
+                title : data.title,
+                amount : data.amount,
+                payment_date : data.payment_date,
+                category : {
+                    cid : data.cid,
+                    name : data.name,
+                    type : data.type,
+                    image : data.image
+                },
+            };
+
+            list.push(history);
+        });
+
+        console.log(list);
+        res.json(list);
+
+    });
+});
+
+// 1-2. 특정 내역 디테일 조회
+router.get('/detail/:id', (req, res, next) => {
+    console.log("find one");
+
+    const list = [];
+    const id = req.params.id;
+    const sql = `select * from (histories as h inner join categories as c on h.category = c.cid) inner join details as d on c.cid = d.did where h.hid = "${id}" order by h.hid asc`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+
+        result.map((data) => {
+            let { ...detail } = {
+                hid : data.hid,
+                title : data.title,
+                amount : data.amount,
+                payment_date : data.payment_date,
+                category : {
+                    cid : data.cid,
+                    name : data.name,
+                    type : data.type,
+                    image : data.image
+                },
+                isfixed : data.isfixed,
+                method : data.method,
+                memo : data.memo,
+            }
+            
+            list.push(detail);
+
+        });
+
+        console.log(list);
+        res.json(list);
+    });
+});
+
+// 1-3. 월별 내역 조회
+router.get('/:ym', (req, res, next) => {
+    console.log("find month");
+
+    const list = [];
+    const date = req.params.ym;
+    const sql = `select * from histories as h inner join categories as c on h.category = c.cid where payment_date like "${date}%" order by h.hid asc`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+
+        result.map((data) => {
+            let { ...history } = {
+                hid : data.hid,
+                title : data.title,
+                amount : data.amount,
+                payment_date : data.payment_date,
+                category : {
+                    cid : data.cid,
+                    name : data.name,
+                    type : data.type,
+                    image : data.image
+                }
+            }
+            
+            list.push(history);
+        });
+
+        console.log(list);
+        res.json(list);
+    });
+});
+
+// 1-4. 연별 수입/지출 금액 조회
+router.get('/amount/:date', (req, res, next) => {
+    console.log('check amount');
+
+    const date = req.params.date;
+    const sql = `select * from histories as h inner join categories as c on h.category = c.cid where payment_date like "${date}%" order by h.hid asc`;
+
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+
+        const income = result
+        .filter(data => {return data.type == '수입'})
+        .reduce((acc, data) => {return acc += data.amount}, 0);
+
+        const expend = result
+        .filter(data => {return data.type == '지출'})
+        .reduce((acc, data) => {return acc += data.amount}, 0);
+        
+        let { ...amount } = {
+            income,
+            expend,
+        };
+
+        console.log(income, expend);
+        res.json(amount);
     })
-})
+});
 
 // 2. 내역 추가
 router.post('/', (req, res, next) => {
